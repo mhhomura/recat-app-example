e58d099622da8f50ed5a42b3feee48227fc607cb
Matheus Homura <matheushomura54@gmail.com>
2024-01-10 13:33:02 -0300

    .

--- src/Pages/Test/index.jsx
+++ src/Pages/Test/index.jsx
@@ -11,8 +11,8 @@ const TestPage = () => {
             <iframe src="https://allchat-bot.kub.app.builderall.io/widget/u0a2f4v4ud104w4uvu81/iframe" width="600" height="600" style={{ border: 'none' }}></iframe> */}
             {/* <iframe src="https://allchat-bot.builderall.com/widget/u0a2f4v4ud104w4uvu81/iframe" width="600" height="600" style={{border: "none"}}></iframe>
             <script src='https://bot.tayon.ai/widget/u0a2f4v4ud104w4uvu81/twidget.js'></script> */}
-            <script src='https://allchat-bot.kube.prod.builderall.io/widget/8DQqw91OYLkiyXiYNzSJ/twidget.js'></script>
-            <iframe src="https://allchat-bot.kube.prod.builderall.io/widget/8DQqw91OYLkiyXiYNzSJ/iframe" width="600" height="600" style={{border: "none"}}></iframe>
+            <iframe src="https://allchat-bot.kub.app.builderall.com/widget/8DQqw91OYLkiyXiYNzSJ/iframe" width="600" height="600"  style={{ border: 'none' }}></iframe>
+            <script src='https://allchat-bot.kub.app.builderall.com/widget/8DQqw91OYLkiyXiYNzSJ/twidget.js'></script>
         </Container>
     )
 }
