df591bc87e9c77b672d5ed434be15e9c8546fc41
Matheus Homura <matheushomura54@gmail.com>
2024-06-06 11:58:37 -0300

    chore: test

--- src/Pages/Test/index.jsx
+++ src/Pages/Test/index.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Helmet } from 'react-helmet';
 
 import {
     Container,
@@ -11,8 +10,7 @@ const TestPage = () => {
 
     return (
         <Container>
-            <script src='https://allchat-bot.kub.app.builderall.io/widget/u0a2f4v4ud104w4uvu81/twidget.js'></script>
-            <iframe src="https://allchat-bot.kub.app.builderall.io/widget/u0a2f4v4ud104w4uvu81/iframe" width="600" height="600" style={{border: "none"}}></iframe>
+           
         {/*  <iframe src="https://allchat-bot.kub.app.builderall.io/widget/8DQqw91OYLkiyXiYNzSJ/iframe" width="600" height="600" style={{border: "none"}}></iframe> */}
         </Container>
     )
